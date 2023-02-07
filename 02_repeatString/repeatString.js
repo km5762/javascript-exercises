cdc56247ba2a6ebe6e71d877882bb74b127e9191
km5762 <km5762@DESKTOP-587P2TU>
2023-02-06 21:33:19 -0500

    Add error handling to repeatstring

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
 const repeatString = function(word, n) {
-    return word.repeat(n);
+    if (n >= 0) {
+        return word.repeat(n);
+    } else
+        return 'ERROR';
 };
 
 // Do not edit below this line
