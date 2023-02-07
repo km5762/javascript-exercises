d88fb3e54be766cf82f3218a7fa8b9d65f2cd270
km5762 <km5762@DESKTOP-587P2TU>
2023-02-07 12:54:30 -0500

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(lower, upper) {
+    let start = Math.min(lower, upper);
+    let end = upper;
+    if (start == upper) {
+        end = lower;
+    } 
+    let sum = 0;
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
