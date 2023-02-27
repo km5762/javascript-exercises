7efa0f4f695dac1f4cb8420eb7f7eba41faf589e
km5762 <km5762@DESKTOP-587P2TU>
2023-02-27 14:13:00 -0500

    finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, a) => sum + a, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((product, a) => product * a, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let currentFact = 1;
+	for (let i = 1; i <= a; i++) {
+    currentFact *= i;
+  }
+  return currentFact;
 };
 
 // Do not edit below this line
