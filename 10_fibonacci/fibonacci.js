99f0909b8bf81261345c581ea52b04e411b0dd61
km5762 <km5762@DESKTOP-587P2TU>
2023-02-27 16:48:01 -0500

    Finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
-};
+    if (number < 0) {
+        return "OOPS";
+    }
+
+    number = Number(number);
+    let a = 0;
+    let b = 1;
+    let c = 1;
+
+    for (let i = 0; i < number - 1; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+};  
 
 // Do not edit below this line
 module.exports = fibonacci;
