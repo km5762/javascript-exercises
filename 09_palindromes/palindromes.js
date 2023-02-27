5b61ae243f4dea1f0c834d299a2c1271df36b8a3
km5762 <km5762@DESKTOP-587P2TU>
2023-02-27 14:28:19 -0500

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[^\w\s\']|_/g, "").toLowerCase().replace(/\s/g, "");
+    for (let i = 0; i < word.length; i++) {
+        const firstLetter = word[i];
+        const secondLetter = word[word.length - 1 - i]
+        
+        if (firstLetter !== secondLetter) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
