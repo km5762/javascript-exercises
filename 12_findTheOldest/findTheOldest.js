82f5250fafb3f86c2076e7c9e15eebb98c36c1c4
km5762 <km5762@DESKTOP-587P2TU>
2023-02-27 17:06:06 -0500

    finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let ages = people.map(person => ((person.yearOfDeath ?? new Date().getFullYear()) - person.yearOfBirth));
+    let index = ages.indexOf(Math.max(...ages));
+    return people[index];
 };
 
 // Do not edit below this line
